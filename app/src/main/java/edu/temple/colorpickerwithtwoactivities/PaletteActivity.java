f0cd55f2dcbf0d3ef83314e5dafb22f276317c19
Dillon Coffman <tug40394@temple.edu>
2019-10-16 11:53:17 -0400

    Added a public method to CanvasFragment that PaletteActivity can use to set its color using the PaletteFragment's implemented ColorSelectedInterface

--- app/src/main/java/edu/temple/colorpickerwithtwoactivities/PaletteActivity.java
+++ app/src/main/java/edu/temple/colorpickerwithtwoactivities/PaletteActivity.java
@@ -1,16 +1,12 @@
 package edu.temple.colorpickerwithtwoactivities;
 
-import android.content.Intent;
 import android.content.res.Resources;
-import android.graphics.Color;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Spinner;
 
 public class PaletteActivity extends AppCompatActivity implements PaletteFragment.ColorSelectedInterface {
+
+    CanvasFragment canvasFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,18 +16,20 @@ public class PaletteActivity extends AppCompatActivity implements PaletteFragmen
         String paletteActionBarTitle = res.getString(R.string.palette_action_bar_title);
         getSupportActionBar().setTitle(paletteActionBarTitle);
 
+        canvasFragment = new CanvasFragment();
         PaletteFragment paletteFragment = PaletteFragment.newInstance(getResources().getStringArray(R.array.colors));
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.container_1, paletteFragment)
+                .add(R.id.container_2, canvasFragment)
                 .commit();
 
-
+        // TODO Implement colorSelected method below and create CanvasFragment, use R.id.container_2 as a container for CanvasFragment here in PaletteActivity
 
     }
 
     @Override
     public void colorSelected(String colorName) {
-
+        canvasFragment.displayColor(colorName);
     }
 }
